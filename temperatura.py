319cf9d90864471b3140a1dfb05fa3ac00ffefa6
Diego Carrizo <dcarrizo015@gmail.com>
2023-09-11 10:17:16 -0300

    subiendo primer archivo desafio victor cortes 3°D

---
+++ temperatura.py
@@ -0,0 +1,10 @@
+def celsisus_a_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
+
+def celsius_a_kelvin(celsius):
+    return celsius + 273.15
+
+if __name__=="__main__":
+    celsius =25
+    fahrenheit = celsisus_a_fahrenheit(celsius)
+    print(f"{celsius} grados celsius son equivalentes a {fahrenheit} grados fahrenheit")
